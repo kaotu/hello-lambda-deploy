b25eab0268f1c86b94e2e336b67ccbaccc6fa503
kaotu <kao-tu@hotmail.co.th>
2020-09-13 19:33:03 +0700

    Update message 🌚

--- deploy-sam/hello_world/app.py
+++ deploy-sam/hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "wow test",
+            "message": "Hello World!",
             # "location": ip.text.replace("\n", "")
         }),
     }
